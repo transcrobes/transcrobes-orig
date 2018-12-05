b420033c325d6f59e2733913fa3b6e30751845fd
Anton Melser <anton@melser.org>
2018-12-05 13:55:35 +0800

    Add gitlab ci
    And set tests to use postgres DB
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- transcrobes/transcrobes/settings/test.py
+++ transcrobes/transcrobes/settings/test.py
@@ -4,7 +4,11 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tc_db',
+        'USER': 'tc_user',
+        'PASSWORD': 'tc_pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
