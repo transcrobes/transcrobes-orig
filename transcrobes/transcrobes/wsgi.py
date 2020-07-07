9aaa30592bd1ce56430bef32a08b21d1ca1b3f4f
Anton Melser <anton@melser.org>
2020-07-07 13:42:17 +0800

    Reorder django wsgi file
    Django has some orderings that the default tools don't like. Make
    CERTAIN that no tools will try and mess with it!
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- transcrobes/transcrobes/wsgi.py
+++ transcrobes/transcrobes/wsgi.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
+# noqa
+# fmt: off
+# pylint: skip-file
+# flake8: noqa
+# WARNING!!! DO NOT allow any reordering of imports here!!! all orderings are EXACTLY how the MUST BE!!!
 
 """
 WSGI config for transcrobes project.
 """
 
-import os
-
-import django
-from django_wsgi.handler import DjangoApplication
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcrobes.settings.container")
-
+import os  # isort:skip
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcrobes.settings.container')
+import django  # isort:skip
 django.setup()
 
-
+from django_wsgi.handler import DjangoApplication  # isort:skip
 application = DjangoApplication()
