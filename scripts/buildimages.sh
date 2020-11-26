64db703d39fe4615309e6695eaa1c49d73dff48a
Anton Melser <anton@melser.org>
2020-11-26 15:32:16 +0800

    Remove docker for building
    Use buildah instead
    
    Signed-off-by: Anton Melser <anton@transcrob.es>

--- scripts/buildimages.sh
+++ scripts/buildimages.sh
@@ -6,11 +6,11 @@ source scripts/runsetup.sh
 TAG=${TRANSCROBES_BUILD_TAG:-$(git describe --tags)}
 
 MAIN_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes:$TAG
-docker build -f images/main/Dockerfile . -t ${MAIN_IMAGE}
-docker push ${MAIN_IMAGE}
+buildah bud -t ${MAIN_IMAGE} -f images/main/Dockerfile .
+buildah push ${MAIN_IMAGE}
 
 STATIC_IMAGE=${TRANSCROBES_DOCKER_REPO}/transcrobes-static:$TAG
 npm run prod
 python3 src/manage.py collectstatic --noinput
-docker build -f images/static/Dockerfile . -t ${STATIC_IMAGE}
-docker push ${STATIC_IMAGE}
+buildah bud -t ${STATIC_IMAGE} -f images/static/Dockerfile .
+buildah push ${STATIC_IMAGE}
